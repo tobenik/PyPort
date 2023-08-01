15a2b929ea05161affc1b2110fdbf9a19ba797b1
tobenik <tobias.nikko@aalto.fi>
2023-08-01 20:08:45 +0300

    Added TransactionManager to Portfolio.

--- pyport/portfolio.py
+++ pyport/portfolio.py
@@ -1,19 +1,37 @@
 from typing import List
 from pyport.transaction import Transaction
+from pyport.transaction_manager import TransactionManager
 
 
 class Portfolio:
-    def __init__(self, name: str, cash_balance: float = 0):
+    def __init__(self, name: str, transaction_manager: TransactionManager, starting_balance: float = 0):
         self.name = name
-        self.cash_balance = cash_balance
+        self.start_balance = starting_balance
+        self.current_balance = starting_balance
         self.transactions: List[Transaction] = []
+        self.tm = transaction_manager
+
+    def refresh(self):
+        self.fetch_transactions()
+        self.count_balance()
+
+    def fetch_transactions(self):
+        self.transactions = self.tm.read_from_csv()
+    
+    def count_balance(self):
+        balance = self.start_balance
+        for t in self.transactions:
+            balance -= t.amount
+        self.current_balance = balance
 
     def add_transaction(self, transaction: Transaction) -> None:
         self.transactions.append(transaction)
-        self.cash_balance -= transaction.amount
-    
-    def print_transactions(self):
+        self.current_balance -= transaction.amount
+
+    def print_info(self):
+        separator = "\n- * - * - * - *\n"
         holdings = ""
         for t in self.transactions:
-            holdings += f"{t.security.name}: {t.amount} | {t.date}"
-        print(holdings)
+            holdings += f"{t.security.name}: {t.amount} | {t.date} \n"
+        print("HOLDINGS", holdings, sep=separator)
+        print("BALANCE", self.current_balance, sep=separator)
