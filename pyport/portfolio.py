44bb8a97cf1b85577a5f4b8c441524b45562023f
tobenik <tobias.nikko@aalto.fi>
2023-08-02 11:18:50 +0300

    Revamped Portfolio

--- pyport/portfolio.py
+++ pyport/portfolio.py
@@ -1,32 +1,29 @@
 from typing import List
 from pyport.transaction import Transaction
-from pyport.transaction_manager import TransactionManager
+from pyport.transaction_file_manager import TransactionFileManager
 
 
 class Portfolio:
-    def __init__(self, name: str, transaction_manager: TransactionManager, start_balance: float = 0):
+    def __init__(self, name: str, transaction_file_manager: TransactionFileManager, start_balance: float = 0):
         self.name = name
+        self.tfm = transaction_file_manager
         self.start_balance = start_balance
-        self.current_balance = start_balance
-        self.transactions: List[Transaction] = []
-        self.tm = transaction_manager
-
-    def refresh(self):
-        self.fetch_transactions()
-        self.count_balance()
+        self.transactions: List[Transaction] = self.fetch_transactions()
+        self.current_balance = self.compute_balance()
 
     def fetch_transactions(self):
-        self.transactions = self.tm.read_from_csv()
-    
-    def count_balance(self):
+        return self.tfm.read_transactions()
+
+    def compute_balance(self):
         balance = self.start_balance
         for t in self.transactions:
             balance -= t.amount
-        self.current_balance = balance
+        return balance
 
     def add_transaction(self, transaction: Transaction) -> None:
         self.transactions.append(transaction)
         self.current_balance -= transaction.amount
+        self.tfm.add_transaction(transaction=transaction)
 
     def print_info(self):
         separator = "\n- * - * - * - *\n"
