a1247786f0e73cc63758ba7b1ae96e70f5ae3ea7
tobenik <tobias.nikko@aalto.fi>
2023-08-02 11:17:24 +0300

    Misc.

--- main.py
+++ main.py
@@ -2,16 +2,15 @@ from datetime import datetime
 from pyport.portfolio import Portfolio
 from pyport.security import Security
 from pyport.transaction import Transaction
-from pyport.transaction_manager import TransactionManager
+from pyport.transaction_file_manager import TransactionFileManager
 
 
 def main():
-    tm = TransactionManager(file_path='data/ex_transactions.csv')
+    tm = TransactionFileManager(file_path='data/ex_transactions.csv')
     portfolio = Portfolio("MyPortfolio", tm, start_balance=20000)
     # buy_aapl = Transaction(amount=15000, security=Security(
     #     "AAPL"), date=datetime(2023, 1, 1))
     # portfolio.add_transaction(transaction=buy_aapl)
-    portfolio.refresh()
     portfolio.print_info()
 
 
