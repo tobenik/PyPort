58c42c0e692066111047d783153397e35f970977
tobenik <tobias.nikko@aalto.fi>
2023-08-02 11:17:48 +0300

    Changed TM to TFM

---
+++ pyport/transaction_file_manager.py
@@ -0,0 +1,49 @@
+import csv
+import os
+from datetime import datetime
+from typing import List
+from pyport.security import Security
+from pyport.transaction import Transaction
+
+
+class TransactionFileManager:
+    def __init__(self, file_path: str) -> None:
+        self.file_path = file_path
+        self.headers: List[str] = ["date", "name", "amount"]
+        self.initialize_file()
+
+    def initialize_file(self):
+        if self.is_empty():
+            with open(self.file_path, 'a') as csvFile:
+                writer = csv.DictWriter(csvFile, fieldnames=self.headers)
+                writer.writeheader()
+
+    def is_empty(self):
+        return os.stat(self.file_path).st_size == 0
+
+    def clear_transactions(self) -> None:
+        open(self.file_path, 'w').close()
+        self.initialize_file()
+
+    def read_transactions(self) -> List[Transaction]:
+        transactions = []
+        with open(self.file_path, 'r') as csvFile:
+            reader = csv.DictReader(csvFile)
+            for row in reader:
+                amount = float(row['amount'])
+                security_name = row['name']
+                date_str = row['date']
+
+                date = datetime.strptime(date_str, '%Y-%m-%d %H:%M:%S')
+                security = Security(name=security_name)
+                transaction = Transaction(
+                    amount=amount, security=security, date=date)
+
+                transactions.append(transaction)
+        return transactions
+
+    def add_transaction(self, transaction: Transaction) -> None:
+        with open(self.file_path, 'a') as csvFile:
+            writer = csv.DictWriter(csvFile, fieldnames=self.headers)
+            transaction_dict = transaction.get_dict()
+            writer.writerow(transaction_dict)
