7d766bb206d5226a4189a24d9a8488e021515f4e
tobenik <tobias.nikko@aalto.fi>
2023-08-02 18:07:39 +0300

    Moved print_info to main.py

--- main.py
+++ main.py
@@ -1,11 +1,32 @@
 from pyport.portfolio import Portfolio
+from pyport.transaction import Transaction
 from pyport.transaction_file_manager import TransactionFileManager
+
+def print_info(portfolio: Portfolio) -> None:
+        def print_header(header: str) -> None:
+            LINEWIDTH = 50
+            SECTION_PATTERN = "-"
+            filling = f"{(LINEWIDTH-len(header))//2 * SECTION_PATTERN}"
+            section_header = f"{filling}{header}{filling}"
+            print(f"{section_header}{'-' if len(section_header) % 2 == 1 else ''}")
+
+        transactions_str = ""
+        for t in portfolio.get_transactions():
+            d = t.get_transaction_date()
+            s = t.get_security()
+            q = t.get_quantity()
+            p = t.get_price()
+            transactions_str += f"{s}\t|\t{q} at {p}€\t|\t{d}\n"
+        print_header("BALANCE")
+        print(portfolio.get_balance())
+        print_header("TRANSACTIONS")
+        print(transactions_str)
 
 
 def main():
     tm = TransactionFileManager(file_path='data/ex_transactions.csv')
     portfolio = Portfolio("MyPortfolio", tm, start_balance=20000)
-    portfolio.print_info()
+    print_info(portfolio)
 
 
 if __name__ == "__main__":
--- pyport/portfolio.py
+++ pyport/portfolio.py
@@ -27,29 +27,9 @@ class Portfolio:
         return self.__current_balance
     
     def get_transactions(self):
-        return [t.get_dict() for t in self.__transactions]
+        return [t for t in self.__transactions]
 
     def add_transaction(self, transaction: Transaction) -> None:
         self.__transactions.append(transaction)
         self.__current_balance -= transaction.get_total()
         self.__tfm.add_transaction(transaction=transaction)
-
-    def print_info(self) -> None:
-        def print_header(header: str) -> None:
-            LINEWIDTH = 50
-            SECTION_PATTERN = "-"
-            filling = f"{(LINEWIDTH-len(header))//2 * SECTION_PATTERN}"
-            section_header = f"{filling}{header}{filling}"
-            print(f"{section_header}{'-' if len(section_header) % 2 == 1 else ''}")
-
-        transactions_str = ""
-        for t in self.__transactions:
-            d = t.get_transaction_date()
-            s = t.get_security()
-            q = t.get_quantity()
-            p = t.get_price()
-            transactions_str += f"{s}\t|\t{q} at {p}€\t|\t{d}\n"
-        print_header("BALANCE")
-        print(self.get_balance())
-        print_header("TRANSACTIONS")
-        print(transactions_str)
